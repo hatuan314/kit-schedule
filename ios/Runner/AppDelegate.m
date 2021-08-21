49aa46c469ccab82447940aa483da52fa500bb16
tuanha <tuanha@acaziasoft.com>
2021-08-21 20:06:45 +0700

    config firebase

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -2,10 +2,12 @@
 #include "GeneratedPluginRegistrant.h"
 
 @implementation AppDelegate
+@import Firebase;
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [FIRApp configure];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
