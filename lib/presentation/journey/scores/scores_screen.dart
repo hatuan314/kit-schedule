663b7f0bb8c86789db1badc754255272463c9875
Trần Trúc Diệp <trucdiep2626@gmail.com>
2021-08-28 13:08:49 +0700

    update my scores screen

--- lib/presentation/journey/scores/scores_screen.dart
+++ lib/presentation/journey/scores/scores_screen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:schedule/presentation/themes/theme_colors.dart';
+import 'package:schedule/presentation/themes/theme_text.dart';
+
 class ScoresScreen extends StatelessWidget {
   const ScoresScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.secondColor,
+      appBar: AppBar(
+        title: Text(
+          'My Scores',
+          style: ThemeText.titleStyle.copyWith(color: AppColor.secondColor),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColor.personalScheduleColor2,
+      ),
+      body: Center(
+        child: Text(
+          'This feature is lauching soon',
+          style: ThemeText.titleStyle2
+              .copyWith(color: AppColor.personalScheduleColor2),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/widget/personal_schedule_element_widget.dart
+++ lib/presentation/widget/personal_schedule_element_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:schedule/presentation/themes/theme_border.dart';
+import 'package:schedule/presentation/themes/theme_colors.dart';
 import 'package:schedule/presentation/themes/theme_text.dart';
 import 'package:schedule/models/model.dart';
 import 'package:schedule/presentation/widget/widgets_constants.dart';
@@ -28,7 +29,7 @@ class PersonalScheduleElementWidget extends StatelessWidget{
                 alignment: Alignment.center,
                 child: Text(
                   '${schedule.timer}',
-                  style: ThemeText.textStyle
+                  style: ThemeText.textStyle.copyWith(color: AppColor.signInColor)
               ),
             )),
             Expanded(
@@ -45,12 +46,17 @@ class PersonalScheduleElementWidget extends StatelessWidget{
                   children: <Widget>[
                     Text(
                       '${schedule.name}',
-                      style:  ThemeText.textStyle,
+                      maxLines: 1,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style:  ThemeText.textStyle.copyWith(color: AppColor.signInColor),
                     ),
                     Text(
-                      '${schedule.note}',
-                      style:  ThemeText.textStyle.copyWith(fontWeight: FontWeight.normal),
-                      maxLines: 5,
+                      schedule.note==null?'':schedule.note as String,
+                      style:  ThemeText.textStyle.copyWith(fontWeight: FontWeight.normal, color: AppColor.signInColor),
+                      maxLines: 1,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
                     ),
                   ],
                 ),
