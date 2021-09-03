01937d985f772b3441bcf6f6582f140394f550b3
Trần Trúc Diệp <trucdiep2626@gmail.com>
2021-09-03 17:02:07 +0700

    fix ui

--- lib/presentation/journey/scores/scores_screen.dart
+++ lib/presentation/journey/scores/scores_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:schedule/presentation/journey/home/calendar_tab_constants.dart';
+import 'package:schedule/presentation/journey/scores/scores_constants.dart';
 import 'package:schedule/presentation/themes/theme_colors.dart';
 import 'package:schedule/presentation/themes/theme_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ScoresScreen extends StatelessWidget {
   const ScoresScreen({Key? key}) : super(key: key);
@@ -9,21 +13,38 @@ class ScoresScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.secondColor,
-      appBar: AppBar(
-        title: Text(
-          'My Scores',
-          style: ThemeText.titleStyle.copyWith(color: AppColor.secondColor),
+
+      body:
+      SafeArea(
+        child:
+        Padding(
+        padding: EdgeInsets.symmetric(
+        horizontal: CalendarTabConstants.paddingHorizontal),
+    child:Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10.h,
+            ),
+            Text( ScoresConstants.myScoresTxt,
+              style: ThemeText.headerStyle2.copyWith(fontSize: 18.h),
+            ),
+
+            Container(
+              height: MediaQuery.of(context).size.height-127,
+              child: Center(
+                child: Text(
+                  'This feature is lauching soon',
+                  style: ThemeText.titleStyle2
+                      .copyWith(color: AppColor.thirdColor),
+                ),
+              ),
+            ),
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: AppColor.personalScheduleColor2,
-      ),
-      body: Center(
-        child: Text(
-          'This feature is lauching soon',
-          style: ThemeText.titleStyle2
-              .copyWith(color: AppColor.personalScheduleColor2),
-        ),
-      ),
+      )
+      )
+      /**/
     );
   }
 }
