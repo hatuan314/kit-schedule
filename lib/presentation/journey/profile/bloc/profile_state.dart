f61e6904715c758f7aa55a160570c14c734ada80
tuanha <tuanha@acaziasoft.com>
2021-09-05 10:42:44 +0700

    Fix confict

--- lib/presentation/journey/profile/bloc/profile_state.dart
+++ lib/presentation/journey/profile/bloc/profile_state.dart
@@ -3,7 +3,6 @@ import 'package:equatable/equatable.dart';
 class ProfileState extends Equatable {
   final String username;
   final bool hasNoti;
-<<<<<<< HEAD
   final bool? isEnglish;
 
   ProfileState({required this.username, required this.hasNoti, this.isEnglish});
@@ -16,16 +15,4 @@ class ProfileState extends Equatable {
   @override
   // TODO: implement props
   List<Object?> get props => [this.username, this.hasNoti,this.hasNoti];
-=======
-
-  ProfileState({required this.username, required this.hasNoti});
-
-  ProfileState update({ String? username,bool? hasNoti}) =>
-      ProfileState(username: username?? this.username,
-      hasNoti: hasNoti?? this.hasNoti);
-
-  @override
-  // TODO: implement props
-  List<Object?> get props => [this.username, this.hasNoti];
->>>>>>> 1943361b048e8fc37b501065a3f7083f81ce4c74
 }
