6ba6ce47b549bec0a77bc9e06ace903fe6be374c
Trần Trúc Diệp <trucdiep2626@gmail.com>
2021-11-07 09:53:06 +0700

    add screenutils and fix font weight

---
+++ lib/presentation/journey/introduction/introduction_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class IntroductionConstants{
+  static double padding= 20.h;
+  static double iconSize=80.sp;
+}
--- lib/presentation/journey/introduction/introduction_screen.dart
+++ lib/presentation/journey/introduction/introduction_screen.dart
@@ -4,14 +4,17 @@ import 'package:schedule/presentation/themes/theme_colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:schedule/presentation/themes/theme_text.dart';
 import 'package:schedule/service/share_service.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'introduction_constants.dart';
 
 class IntroductionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: IntroductionConstants.padding),
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
@@ -20,47 +23,26 @@ class IntroductionScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(IntroductionConstants.padding),
                 child: Container(
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(IntroductionConstants.padding),
                   decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: AppColor.personalScheduleBackgroundColor),
                   child: Icon(
                     Icons.event,
                     color: AppColor.personalScheduleColor2,
-                    size: 80.0,
+                    size: IntroductionConstants.iconSize,
                   ),
                 ),
               ),
               Text(
                 AppLocalizations.of(context)!.introduction,
-                style: ThemeText.titleStyle2
-                     .copyWith(color: AppColor.personalScheduleColor2)
-                ,
+                style: ThemeText.labelStyle.copyWith(
+                  color: AppColor.personalScheduleColor2,
+                ),
                 textAlign: TextAlign.center,
               ),
-              // Padding(
-              //   padding:   EdgeInsets.only(top: 20.0),
-              //   child: Row(
-              //     children: [
-              //       Expanded(
-              //           flex: 1,
-              //           child: navBarItem(false, Icons.date_range,  )),
-              //       Expanded(
-              //           flex: 1,
-              //           child: navBarItem(
-              //               false, Icons.score_outlined,  )),
-              //       Expanded(
-              //           flex: 1,
-              //           child:
-              //           navBarItem(false, Icons.content_paste,  )),
-              //       Expanded(
-              //           flex: 1,
-              //           child: navBarItem(true, Icons.person)),
-              //     ],
-              //   ),
-              // )
             ],
           ),
         ),
@@ -71,30 +53,13 @@ class IntroductionScreen extends StatelessWidget {
           _shareService.setIsFirstRun(false);
           Navigator.pushReplacementNamed(context, '/home');
         },
-      
         child: Container(
-          //color: AppColor.personalScheduleBackgroundColor,
-        //  margin: EdgeInsets.all(20.0),
-            padding: EdgeInsets.all(20.0),
-            child: Text(AppLocalizations.of(context)!.ok,
-            style: ThemeText.buttonLabelStyle.copyWith(color: AppColor.personalScheduleColor2),)),
-      ),
-    );
-  }
-  Widget navBarItem( bool isSelected, IconData icon, ) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 2.0,
-          color: isSelected ? AppColor.personalScheduleColor2: AppColor.secondColor
-        )
-      ),
-      height: MainScreenConstants.navBarHeight,
-      child: Icon(
-        icon,
-        color: isSelected
-            ? AppColor.fourthColor
-            : AppColor.primaryColor,
+            padding: EdgeInsets.all(IntroductionConstants.padding),
+            child: Text(
+              AppLocalizations.of(context)!.ok,
+              style: ThemeText.buttonLabelStyle
+                  .copyWith(color: AppColor.personalScheduleColor2),
+            )),
       ),
     );
   }
